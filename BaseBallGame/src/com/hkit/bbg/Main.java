beb50cb39a5484820d4c2a2314eb123ffc4136c7
석영수 <@DESKTOP-3THF9JL>
2019-10-24 12:43:45 +0900

    입력받기

--- BaseBallGame/src/com/hkit/bbg/Main.java
+++ BaseBallGame/src/com/hkit/bbg/Main.java
@@ -1,5 +1,7 @@
 package com.hkit.bbg;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		final int NO_COUNT = 3;           
@@ -7,6 +9,7 @@ public class Main {
 		int[] randomNoArray = new int[NO_COUNT];
 		int[] userNoArray = new int[NO_COUNT];
 		
+		//랜덤값 3개 추출(중복x)
 		for(int i=0; i<NO_COUNT; i++) {
 			int randomValue = (int)(Math.random() * 9) + 1;
 			               //i+1
@@ -21,9 +24,33 @@ public class Main {
 				}
 			}
 		}
+		//콘솔에서 값을 입력받기 위한 객체
+		/*
+		Scanner scan = new Scanner(System.in);
+		System.out.print("값을입력해주세요 :");
+		int value1 = scan.nextInt();
 		
-		for(int item : randomNoArray) {
-			System.out.println(item);
+		
+		System.out.print("값을입력해주세요 :");
+		int value2 = scan.nextInt();
+		
+		
+		System.out.print("값을입력해주세요 :");
+		int value3 = scan.nextInt();
+		System.out.printf("value1 : %d\nvalue2 : %d\nvalue3 : %d\n", value1, value2, value3);
+		*/
+		
+		Scanner scan = new Scanner(System.in);
+		
+		for(int i=0; i<NO_COUNT; i++) {
+			System.out.print("값을입력해주세요 :");
+			int value = scan.nextInt();
+			userNoArray[i] = value;
+		}
+		scan.close();
+		
+		for(int item : userNoArray) {
+			System.out.println("valu : " + item);
 		}
 	
 	}
